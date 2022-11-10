fbe08acde27a38f65d900c9840b6e7ac2a4e625b
Luis Fernando <luis.nando.acarau@gmail.com>
2022-11-10 15:33:26 -0300

    Tela de Chat

--- app/src/main/java/br/ufc/quixada/myapplication/AmigosActivity.java
+++ app/src/main/java/br/ufc/quixada/myapplication/AmigosActivity.java
@@ -1,9 +1,13 @@
 package br.ufc.quixada.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,15 +24,15 @@ import java.util.List;
 import br.ufc.quixada.myapplication.model.Usuario;
 
 public class AmigosActivity extends AppCompatActivity {
-    ArrayAdapter adapter;
-    ArrayList<Usuario> listaAmigos;
-    ListView listView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_amigos);
 
-        listaAmigos = new ArrayList<>();
+        ArrayList<Usuario> listaAmigos = new ArrayList<>();
         Usuario u = new Usuario("Fulano", "5454", "2121", "f@gmail.com", "senha123");
         listaAmigos.add(u);
         listaAmigos.add(u);
@@ -49,11 +53,18 @@ public class AmigosActivity extends AppCompatActivity {
 //                    }
 //                });
 
-        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaAmigos);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listaAmigos);
 //        RecyclerView recyclerView = findViewById(R.id.recycler);
 //        recyclerView.setAdapter(adapter);
-        listView = (ListView) findViewById(R.id.listView);
+        ListView listView = (ListView) findViewById(R.id.list_view_amigos);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                Intent intent = new Intent(AmigosActivity.this, ChatActivity.class);
+                startActivity(intent);
+            }
+        });
         buscarUsuarios();
     }
 
